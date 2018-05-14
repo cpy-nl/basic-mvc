49a74bfd1183e489684768740ba67a724b8cdf9e
cris <cris.pijper@gmail.com>
2018-05-14 10:14:23 +0200

    moved public static var

--- classes/Route.php
+++ classes/Route.php
@@ -1,8 +1,7 @@
 <?php
 
-public static $validRoutes = array();
-
 class Route {
+  public static $validRoutes = array();
   public static function set($route, $function) {
     self::$validRoutes[] = $route;
     if($_GET['url'] == $route) {
