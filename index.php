e51263d9659d126e0b5842dfb21afdd1de21df5e
cris <cris.pijper@gmail.com>
2018-05-15 17:22:42 +0200

    refactor using spl autoloader

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 require_once 'Routes.php';
 
-function __autoload($class_name) {
-  if(file_exists('./classes/' . $class_name . '.php')) {
-    require_once './classes/' . $class_name . '.php';
-  } else if (file_exists('./Controllers/' . $class_name . '.php')) {
-    require_once './Controllers/' . $class_name . '.php';
+function autoloader($class) {
+  if(file_exists('./classes/' . $class . '.php')) {
+    require_once './classes/' . $class . '.php';
+  } else if (file_exists('./Controllers/' . $class . '.php')) {
+    require_once './Controllers/' . $class . '.php';
   }
 }
+spl_autoload_register('autoloader');﻿
 
 ?>
